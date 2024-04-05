36fecac71fad758b0d1cd3290142ec5e5ef0bb9f
Neoanna <143848719+Neoanna@users.noreply.github.com>
2024-04-05 15:49:53 -0700

    start

--- text.js
+++ text.js
@@ -1,8 +1,129 @@
+[
+{"intro":"You awake in an empty classroom, you are Kazuki Ryuusen. It’s the afternoon with the soft golden light reflecting off your desk and warm breeze. It’s peaceful.. It’s always been peaceful. You realize that you’ve always gone with the flow, someone in the background, not someone to stand out. You are someone of no significance, would anyone even know if you just suddenly disappeared? Who would go looking for you? What impact have you made? …..It’s late, go home, get some rest, you can think about it later.. It’ll all be ok.."},
+{"classroom":"You’re about to leave the classroom, the serenity of it all and the thoughts. As you reach for the doorknob, you lose grip and.. Teleport? It feels like falling.."},
+{"morph":"Wait, what’s that over there? Three spirits? They look like an elf, fairy, and a regular human? Look, they’re trying to merge with you, Kazuki, merging with them will guarantee a safe teleport."},
+{"elf":"You have longer ears, and you feel a surge of power."},
+{"fairy":"You have wings and.. antennas..? You sense a greater feeling towards nature."},
+{"human":"You’re the same as you were, maybe you just didn’t want to be a cool mythical creature but alright."},
+{"ATM":"Something speaks to you in your head, oh wait, this is in your head, haha! Hello, I am system, your Auto Technical Memory, or ATM for short, I will guide you on this adventure. You feel ground beneath you, it’s a soft red carpet. As your eyes trail this carpet along the marble floor you find a king at the end of it.. He seems powerful and cocky. Then, he speaks, “Kazuki Ryuusen from across the universe, I am King Idgris, please follow my commands and train to fight in this war, I promise you great glory and a safe return home.” A chance to become someone useful? A chance to become a hero? No. An opportunity to save this world."},
+{"firstDeath":"End: 50/50"},
+{"secondDeath":"The king was not pleased with your answers, he has no use for you so he signals his guard to kill you. As a sword pierce your heart, you die feeling the sharp pain of the blade."},
+{"king":"The king is pleased and tells one of his knights to guide you to the knights quarters. You finally got the opportunity to be someone, and you took it. The first thing you do when you’re at the quarters is get into gear and spar a rookie."},
+{"strike":"They blocked your attack and slashed at you! You’re injured! You are damaged and he has the upper hand, the moment he lunged towards you, you lost."},
+{"defend":"They striked first but it was pretty weak, maybe they’re getting a feel of the battle? You then strike against his wooden sword and he blocks and hits you. You lost."},
+{"aftermath":"Wow, you lost to a rookie, maybe they chose the wrong hero. Kidding of course, don’t lose hope just because you lost once in something you never experienced before. Ok, let’s train. First thing we should do is fight some slime, they’re weak but you’ll get a good feel of the battlefield."},
+{"slime":"The slimes are cute, but don’t fall for it, they’re extremely invasive species that adapt to any environment and eat everything. Even life forces if there’s enough of them. Remember, you’re only here to gain experience. Let’s start slow and follow the trail to the slime patch."},
+{"follow":"Wow, there’s a lot of slimes here, don’t worry though, even a kid could fight them. Just be careful where you step and stay on guard of your surroundings."},
+{"offPath":"Wait, where are you going? You haven’t even been in this game for that long and you’re already going where you want? I mean sure I might be a voice in your head but you should still listen to me! Wait, is that a cave? Don’t go in there!"},
+{"cave":"The moment you step into the cave you hear a low growl. This is your last warning!"},
+{"further":"You follow the cave’s wall deeper into the darkness, and then, a dim light at the end. You peek around the corner to find a dragon wide awake with gold, diamonds, rubies, and sapphires surrounding it. It spots you, but it’s too late to run back now, the dragon is fast and blows fire in the cave. You are burnt to ashes."},
+{"secondEnding":"Ending: Died by not listening to the ATM."},
+{"killSlime":"You slay the slimes and get a better understanding of fighting. You sense a better feeling of the sword, the flow of your strikes and slashes are becoming one with your body and movement. Maybe you do have a talent for swordsmanship, anywho, let’s go fight the dragon."},
+{"fightDragon":"You are directed by ATM to the dragon’s cave and proceed further into the cave. You peek around the corner where a dim light sits and see a dragon at the end of it surrounded by priceless gems and minerals. The dragon is in a slumber, this is the perfect chance to attack. You quietly sneak up close enough to the dragon, and then strike its eyes, the dragon awakes and spins around hitting you with its tail. You’re injured but quickly get to your feet and strike the dragon’s legs, reducing it’s mobility. It then falls forward to the ground and you take one final swing to the neck of the dragon. It’s dead. That dragon took down 3 empires to get it’s fortune, but now it’s your fortune, or at least as much as you can carry. Ok, time to go back to the king."},
+{"refuse":"Too bad."},
+{"shiny":"+10 strength"},
+{"king":"King, 'Welcome back Kazuki! I heard tales of your adventure of slimes and dragons! I can’t believe you actually went from slimes to dragons! I think you’re ready for the war. Go and join today’s troops to the front lines!'"},
+{"remorse":"You’re actually going to remorse it? How heroic.. You bury the dragon and remorse over it."},
+{"troops":"You meet the troops, and there’s like, only 4 people there, counting you.. The group introduces themselves The only girl on the team, “I am Eliza. The warrior, I specialize in dealing a lot of damage.” A handsome long haired knight, “I am Caelus Mal Victor Heinz. The Crown Prince of this Kingdom, I am a healer, it’s nice to meet you Kazuki.” Finally a skinny guy, “I am Bruno. The mage.” Wow, what a group of people... a warrior, healer, Mage, and the Hero. It doesn’t seem you actually have to say anything, they already know who you are."},
+{"princeSpar":"Even though he's a healer, he's quick on his feet, and dashes forward. He's taking the offense."},
+{"attack":"You strike at his hand which makes him drop his sword. You won in a single slash with the blade of your sword pointed at his neck. Caelus smiles, 'You won.. I have always been weak..' He looks like a sad puppy. Why? Because I say so."},
+{"yeahWeak":"Caelus laughs, 'You really don't empathize with me? Aren't you scared of my political power?' It's true, he will be the kind one day.."},
+{"yCare":"Caelus is shocked, 'I mean, I guess so, but what's stopping me from killing you now?' He seems upset."},
+{"mean":"He's silent and looks at the ground."},
+{"sorry":"Caelus laughs, 'No one has ever treated me like an actual person.. Not even my own father, I awlays wanted to be the person he wanted me to be, but you somehow bring me to feel like a human. You remind me of my mother..' I don't know much about the queen, all I know is that she was pretty."},
+{"mom":"Caelus, 'She was king, warm, stubborn, and strong. Everything I wasn't. She fought with a sword, right to the demon king until her last breath..' And now the mood is depressing.."},
+{"queen":"Caelus, 'She was, she really was..' He's smiling now.. You feel a stronger bond with the Crown Prince."},
+{"timeToGo":"Caelus, 'I'll see you on the other side, Kazuki.'"},
+{"block":"You strike his sword to block his slahs and kick his leg. He lost his balance and you were able to apprehend him. Caelus, 'You didn't strike me. Why.' It sounds like more of an order than a question..!"},
+{"crownPrince":"He's disappointed but understands.."},
+{"":""},
+{"":""},
+{"":""},
+{"":""},
+{"":""},
+{"":""},
+{"":""},
+{"":""},
+{"":""},
+{"":""},
+{"":""},
+{"":""},
+{"":""},
+{"":""},
+{"":""},
+{"":""},
+{"":""},
+{"":""},
+{"":""},
+{"":""},
+]
 
 
+let state = {}
 
-function(txtChange){
+const textElement = document.getElementById('text')
+const optionButtonsElement = document.getElementById('opttion-buttons')
+
+function startGame() {
+    state = {}
+    showTextNode(1)
+}
+
+function showTextNode(textNodeIndex){
+ const textNode = textNodes.find(textNode => textNode.id ===
+textNodeIndex)
+textElement.innerText= textNode.text
+while(optionButtonsElement.firstChild) {
+    optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+}
+textNode.options.forEach(option => {
+    if(showOption(option)){
+        const button = document.createElement('button')
+        button.innerText = option.text
+        button.classlist.add('btn')
+        button.addEventListener('click', ()=> selectOption(option))
+        optionButtonsElement.appendChild(button)
+    }
+})
+}
+
+
+function selectOption(option) {
 
 }
 
-// Need a function to change the class, and a class that contains the dialouge.
+const textNodes = [
+    {
+        id:1,
+        text:"",
+        options:[
+            {
+                text:'Fairy',
+                setEmpty: { Fairy: true},
+                nextText:2.1
+            },
+            {
+                text:'Elf',
+                nextTex:2.2
+            },
+            {
+                text:'Human',
+                nextText:2.3
+            }
+        ]
+    },
+    {
+        id:2,
+        text:'You have wings and.. antennas..? You sense a greater feeling towards nature.'
+    },
+    {
+        id:3,
+        text:'You have longer ears, and you feel a surge of power.'
+    },
+    {
+        id:4,
+        text:'You’re the same as you were, maybe you just didn’t want to be a cool mythical creature but alright.'
+    }
+]
+
+startGame()
